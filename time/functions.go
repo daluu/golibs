90c2df60f5325924cda2ae5bc7e82a98762de52e
David Luu <daluu@mistsys.com>
2018-09-04 23:19:43 -0700

    add duration formatting method

--- time/functions.go
+++ time/functions.go
@@ -30,3 +30,33 @@ func UnixTimeParser(unixTime int64) time.Time {
 func UnixMillisec(ts time.Time) int64 {
 	return ts.UnixNano() / 1000 / 1000
 }
+
+func formatDuration(duration time.Duration, decimalPrecision int) string {
+	formatString := "%." + fmt.Sprintf("%d", decimalPrecision) + "f"
+	h := duration.Hours()
+	m := duration.Minutes()
+	s := duration.Seconds()
+	ms := float64(s * 1000)
+	//although, often no need go past ms for timings in real world?
+	us := float64(s * 1000 * 1000)
+	//ns := duration.Nanoseconds()
+	formattedDuration := ""
+	if h > 1 {
+		//highest possible duration we can use, following go convention
+		//don't deal with days, weeks, months, years
+		formattedDuration = fmt.Sprintf(formatString, h) + "h"
+	} else if m > 1 {
+		formattedDuration = fmt.Sprintf(formatString, m) + "m"
+	} else if s > 1 {
+		formattedDuration = fmt.Sprintf(formatString, s) + "s"
+	} else if ms > 1 {
+		//future TODO: regex parse out digits for better precision than this method
+		formattedDuration = fmt.Sprintf(formatString, ms) + "ms"
+	} else if us > 1 {
+		formattedDuration = fmt.Sprintf(formatString, us) + "µs"
+	} else {
+		//lowest possible duration we can use, & can't format decimal, int only
+		formattedDuration = duration.String()
+	}
+	return formattedDuration
+}
